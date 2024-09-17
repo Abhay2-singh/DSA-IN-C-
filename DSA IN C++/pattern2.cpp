23ceca3c72d2a922ad984e0c1b7ae273b1209127
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-09-17 09:51:07 +0530

    Add files via upload

---
+++ DSA IN C++/pattern2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"\nenter the number=";
+    cin>>n;
+
+    int i=1;
+    while (i<=n)
+    {
+        int j=i;
+        while (j<=n)
+        {
+            cout<<"i";
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+        
+    }
+    return 0;
+    
+}
