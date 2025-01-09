4a0dc2abfca3d0e14de5bb45d9958d40311bcea5
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2025-01-09 09:07:25 +0530

    Add files via upload

---
+++ Macro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#define Pi 3.14
+int main(){
+    int r;
+    cout<<"\nenter the radius";
+    cin>>r;
+    double ar=Pi*r*r;
+    cout<<"Araa of circle is="<<ar;
+    return 0;
+}
